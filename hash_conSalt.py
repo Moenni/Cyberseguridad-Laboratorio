499f54c19beb70f5b955563f9e3f94f1e688a961
Moenni <nicolasmoen@live.com>
2026-03-04 16:32:16 -0300

    eliminando archivos innecesarios

--- hash_conSalt.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-# Contraseña original
-password = b"admin123"
-
-# Generar hash con salt automático
-hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-
-print("Hash bcrypt:", hashed)
